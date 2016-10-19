8b7f5149780c97b1170bebd97fffa69e32690ce9
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-10-19 13:48:55 +0300

    test(upgrade/aot_spec): avoid using `<body>` as `$rootElement`

--- modules/@angular/upgrade/test/aot/test_helpers.ts
+++ modules/@angular/upgrade/test/aot/test_helpers.ts
@@ -21,11 +21,17 @@ export function bootstrap(
 }
 
 export function html(html: string): Element {
+  // Don't return `body` itself, because using it as a `$rootElement` for ng1
+  // will attach `$injector` to it and that will affect subsequent tests.
   const body = document.body;
-  body.innerHTML = html;
-  if (body.childNodes.length == 1 && body.firstChild instanceof HTMLElement)
-    return <Element>body.firstChild;
-  return body;
+  body.innerHTML = `<div>${html.trim()}</div>`;
+  const div = document.body.firstChild as Element;
+
+  if (div.childNodes.length === 1 && div.firstChild instanceof HTMLElement) {
+    return div.firstChild;
+  }
+
+  return div;
 }
 
 export function multiTrim(text: string): string {
