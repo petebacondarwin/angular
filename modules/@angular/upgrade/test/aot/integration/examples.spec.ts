a0ccc4adabd7a1c0b453d9411dfa63a7f44b7030
Peter Bacon Darwin <pete@bacondarwin.com>
2016-10-19 11:06:57 +0100

    reformat tests

--- modules/@angular/upgrade/test/aot/integration/examples.spec.ts
+++ modules/@angular/upgrade/test/aot/integration/examples.spec.ts
@@ -1,13 +1,13 @@
-import {Component, Directive, ElementRef, Injector, NgModule, destroyPlatform, Input} from '@angular/core';
+import {Component, Directive, ElementRef, Injector, Input, NgModule, destroyPlatform} from '@angular/core';
+import {async} from '@angular/core/testing';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {UpgradeComponent, UpgradeModule, downgradeComponent} from '@angular/upgrade';
 import * as angular from '@angular/upgrade/src/angular_js';
+
 import {bootstrap, html, multiTrim} from '../test_helpers';
-import {async} from '@angular/core/testing';
 
 export function main() {
-
   describe('examples', () => {
 
     beforeEach(() => destroyPlatform());
@@ -17,64 +17,62 @@ export function main() {
 
     it('should verify UpgradeAdapter example', async(() => {
 
-          // This is wrapping (upgrading) an Angular 1 component to be used in an Angular 2
-          // component
-          @Directive({selector: 'ng1'})
-          class Ng1Component extends UpgradeComponent {
-            constructor(elementRef: ElementRef, injector: Injector) {
-              super('ng1', elementRef, injector);
-            }
-            @Input() title: string;
-          }
+         // This is wrapping (upgrading) an Angular 1 component to be used in an Angular 2
+         // component
+         @Directive({selector: 'ng1'})
+         class Ng1Component extends UpgradeComponent {
+           constructor(elementRef: ElementRef, injector: Injector) {
+             super('ng1', elementRef, injector);
+           }
+           @Input() title: string;
+         }
 
-          // This is an Angular 2 component that will be downgraded
-          @Component({
-            selector: 'ng2',
-            template: 'ng2[<ng1 [title]="name">transclude</ng1>](<ng-content></ng-content>)'
-          })
-          class Ng2Component {
-            @Input('name') nameProp: string;
-          }
+         // This is an Angular 2 component that will be downgraded
+         @Component({
+           selector: 'ng2',
+           template: 'ng2[<ng1 [title]="name">transclude</ng1>](<ng-content></ng-content>)'
+         })
+         class Ng2Component {
+           @Input('name') nameProp: string;
+         }
 
-          // This module represents the Angular 2 pieces of the application
-          @NgModule({
-            declarations: [Ng1Component, Ng2Component],
-            entryComponents: [Ng2Component],
-            imports: [BrowserModule, UpgradeModule]
-          })
-          class Ng2Module {
-            ngDoBootstrap() { /* this is a placeholder to stop the boostrapper from complaining */
-            }
-          }
+         // This module represents the Angular 2 pieces of the application
+         @NgModule({
+           declarations: [Ng1Component, Ng2Component],
+           entryComponents: [Ng2Component],
+           imports: [BrowserModule, UpgradeModule]
+         })
+         class Ng2Module {
+           ngDoBootstrap() { /* this is a placeholder to stop the boostrapper from complaining */
+           }
+         }
 
-          // This module represents the Angular 1 pieces of the application
-          const ng1Module =
-              angular
-                  .module('myExample', [])
-                  // This is an Angular 1 component that will be upgraded
-                  .directive(
-                      'ng1',
-                      () => {
-                        return {
-                          scope: {title: '='},
-                          transclude: true,
-                          template: 'ng1[Hello {{title}}!](<span ng-transclude></span>)'
-                        };
-                      })
-                  // This is wrapping (downgrading) an Angular 2 component to be used in Angular 1
-                  .directive(
-                      'ng2', downgradeComponent({component: Ng2Component, inputs: ['name']}));
+         // This module represents the Angular 1 pieces of the application
+         const ng1Module =
+             angular
+                 .module('myExample', [])
+                 // This is an Angular 1 component that will be upgraded
+                 .directive(
+                     'ng1',
+                     () => {
+                       return {
+                         scope: {title: '='},
+                         transclude: true,
+                         template: 'ng1[Hello {{title}}!](<span ng-transclude></span>)'
+                       };
+                     })
+                 // This is wrapping (downgrading) an Angular 2 component to be used in Angular 1
+                 .directive('ng2', downgradeComponent({component: Ng2Component, inputs: ['name']}));
 
-          // This is the (Angular 1) application bootstrap element
-          // Notice that it is actually a downgraded Angular 2 component
-          const element = html('<ng2 name="World">project</ng2>');
+         // This is the (Angular 1) application bootstrap element
+         // Notice that it is actually a downgraded Angular 2 component
+         const element = html('<ng2 name="World">project</ng2>');
 
-          // Let's use a helper function to make this simpler
-          bootstrap(platformBrowserDynamic(), Ng2Module, element, ng1Module).then(upgrade => {
-            expect(multiTrim(element.textContent))
-              .toEqual('ng2[ng1[Hello World!](transclude)](project)');
-            });
-      }));
+         // Let's use a helper function to make this simpler
+         bootstrap(platformBrowserDynamic(), Ng2Module, element, ng1Module).then(upgrade => {
+           expect(multiTrim(element.textContent))
+               .toEqual('ng2[ng1[Hello World!](transclude)](project)');
+         });
+       }));
   });
-
 }
--- modules/@angular/upgrade/test/aot/integration/injection.spec.ts
+++ modules/@angular/upgrade/test/aot/integration/injection.spec.ts
@@ -1,10 +1,11 @@
-import {NgModule, destroyPlatform, OpaqueToken} from '@angular/core';
+import {NgModule, OpaqueToken, destroyPlatform} from '@angular/core';
+import {async} from '@angular/core/testing';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {UpgradeModule, downgradeInjectable} from '@angular/upgrade';
 import * as angular from '@angular/upgrade/src/angular_js';
+
 import {bootstrap, html} from '../test_helpers';
-import {async} from '@angular/core/testing';
 
 export function main() {
   describe('injection', () => {
@@ -12,60 +13,60 @@ export function main() {
     beforeEach(() => destroyPlatform());
     afterEach(() => destroyPlatform());
 
-      it('should downgrade ng2 service to ng1', async(() => {
-           // Tokens used in ng2 to identify services
-           const Ng2Service = new OpaqueToken('ng2-service');
+    it('should downgrade ng2 service to ng1', async(() => {
+         // Tokens used in ng2 to identify services
+         const Ng2Service = new OpaqueToken('ng2-service');
 
-           // Sample ng1 NgModule for tests
-           @NgModule({
-             imports: [BrowserModule, UpgradeModule],
-             providers: [
-               {provide: Ng2Service, useValue: 'ng2 service value'},
-             ]
-           })
-           class Ng2Module {
-             ngDoBootstrap() {}
-           }
+         // Sample ng1 NgModule for tests
+         @NgModule({
+           imports: [BrowserModule, UpgradeModule],
+           providers: [
+             {provide: Ng2Service, useValue: 'ng2 service value'},
+           ]
+         })
+         class Ng2Module {
+           ngDoBootstrap() {}
+         }
 
-           // create the ng1 module that will import an ng2 service
-           const ng1Module = angular.module('ng1Module', [])
-                                 .factory('ng2Service', downgradeInjectable(Ng2Service));
+         // create the ng1 module that will import an ng2 service
+         const ng1Module =
+             angular.module('ng1Module', []).factory('ng2Service', downgradeInjectable(Ng2Service));
 
-           bootstrap(platformBrowserDynamic(), Ng2Module, html('<div>'), ng1Module).then((upgrade) => {
-            const ng1Injector = upgrade.$injector;
-             expect(ng1Injector.get('ng2Service')).toBe('ng2 service value');
-           });
-         }));
+         bootstrap(platformBrowserDynamic(), Ng2Module, html('<div>'), ng1Module)
+             .then((upgrade) => {
+               const ng1Injector = upgrade.$injector;
+               expect(ng1Injector.get('ng2Service')).toBe('ng2 service value');
+             });
+       }));
 
-      it('should upgrade ng1 service to ng2', async(() => {
-           // Tokens used in ng2 to identify services
-           const Ng1Service = new OpaqueToken('ng1-service');
+    it('should upgrade ng1 service to ng2', async(() => {
+         // Tokens used in ng2 to identify services
+         const Ng1Service = new OpaqueToken('ng1-service');
 
-           // Sample ng1 NgModule for tests
-           @NgModule({
-             imports: [BrowserModule, UpgradeModule],
-             providers: [
-               // the following line is the "upgrade" of an Angular 1 service
-               {
-                 provide: Ng1Service,
-                 useFactory: (i: angular.IInjectorService) => i.get('ng1Service'),
-                 deps: ['$injector']
-               }
-             ]
-           })
-           class Ng2Module {
-             ngDoBootstrap() {}
-           }
+         // Sample ng1 NgModule for tests
+         @NgModule({
+           imports: [BrowserModule, UpgradeModule],
+           providers: [
+             // the following line is the "upgrade" of an Angular 1 service
+             {
+               provide: Ng1Service,
+               useFactory: (i: angular.IInjectorService) => i.get('ng1Service'),
+               deps: ['$injector']
+             }
+           ]
+         })
+         class Ng2Module {
+           ngDoBootstrap() {}
+         }
 
-           // create the ng1 module that will import an ng2 service
-           const ng1Module =
-               angular.module('ng1Module', []).value('ng1Service', 'ng1 service value');
+         // create the ng1 module that will import an ng2 service
+         const ng1Module = angular.module('ng1Module', []).value('ng1Service', 'ng1 service value');
 
-           bootstrap(platformBrowserDynamic(), Ng2Module, html('<div>'), ng1Module).then((upgrade) => {
-             var ng2Injector = upgrade.injector;
-             expect(ng2Injector.get(Ng1Service)).toBe('ng1 service value');
-           });
-         }));
-    });
-
-  }
+         bootstrap(platformBrowserDynamic(), Ng2Module, html('<div>'), ng1Module)
+             .then((upgrade) => {
+               var ng2Injector = upgrade.injector;
+               expect(ng2Injector.get(Ng1Service)).toBe('ng1 service value');
+             });
+       }));
+  });
+}
