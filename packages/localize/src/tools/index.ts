b44aa2fb6b01873db6afa4e0f8b6fc318f4022c8
Pete Bacon Darwin <pete@bacondarwin.com>
2019-11-18 17:16:12 +0000

    refactor(ivy): i18n - expose translation loading API
    This refactoring makes is easier to load translation files
    without having to know the internals of how it is done.

---
+++ packages/localize/src/tools/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+// General API
+export {Diagnostics} from './src/diagnostics';
+// Translation loading API
+export {TranslationFile, TranslationLoader} from './src/translate/translation_files/translation_loader';
+export {SimpleJsonTranslationParser} from './src/translate/translation_files/translation_parsers/simple_json_translation_parser';
+export {TranslationParser} from './src/translate/translation_files/translation_parsers/translation_parser';
+export {Xliff1TranslationParser} from './src/translate/translation_files/translation_parsers/xliff1_translation_parser';
+export {Xliff2TranslationParser} from './src/translate/translation_files/translation_parsers/xliff2_translation_parser';
+export {XtbTranslationParser} from './src/translate/translation_files/translation_parsers/xtb_translation_parser';
